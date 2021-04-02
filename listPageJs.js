1cfbaea3be143e219026b39a194b59b8fadcc7bc
JacobFeat <jakubk198@gmail.com>
2021-04-03 00:27:59 +0200

    add basic search feature v1 and very basic list of suggestions

--- listPageJs.js
+++ listPageJs.js
@@ -1,21 +1,60 @@
 const searchInput = document.querySelector(".search");
+const suggestionsList = document.querySelector(".list-of-suggestions");
 const lookupIcon = document.querySelector(".lookup-icon");
 const lookupAndMap = document.querySelector(".lookup-and-map");
+const suggestionsCity = suggestionsList.children;
 
-
-//expand search input
-searchInput.addEventListener('click', function(){
-  searchInput.classList.add('search-active');
-  lookupIcon.classList.add('lookup-icon-active');
+document.addEventListener('mouseover', (e) => {
+  e.target.className === "search" ? searchInput.classList.add("search-hover") : searchInput.classList.remove("search-hover");
 });
 
-lookupIcon.addEventListener('click', function(){
-  searchInput.classList.add('search-active');
-  lookupIcon.classList.add('lookup-icon-active');
+//expand and collapse search input
+document.addEventListener('click', (e) => {
+  if (e.target.className === "search" || e.target.className === "search search-active" || e.target.className === "search search-hover") {
+    searchInput.classList.add('search-active');
+    lookupIcon.classList.add('lookup-icon-active');
+  } else {
+    searchInput.classList.remove('search-active');
+    lookupIcon.classList.remove('lookup-icon-active');
+    searchInput.value = "";
+  }
 });
 
-//hover search icon
-lookupAndMap.addEventListener('mouseover', function(e){
-  e.target.className ==  "material-icons lookup-icon" ? searchInput.classList.add("search-hover") : searchInput.classList.remove("search-hover");
+//search city function
+let cities = [];
 
-})
+fetch('pl.json')
+    .then(blob => blob.json())
+    .then(data => cities.push(...data));
+
+function findMatches(cityToSearch, cities){
+  return cities.filter((place) => {
+      const regex = new RegExp(cityToSearch, 'gi');
+      return place.city.match(regex);
+  })
+};
+
+function displayMatches(){
+  const matchCities = findMatches(this.value, cities);
+  const html = matchCities.map(place => {
+    const regex = new RegExp(this.value, 'gi');
+    const cityName = place.city.replace(regex, this.value);
+
+    return `
+    <li>
+      <span class = "name">${cityName}</span>
+    </li>
+    `;
+  }).join("");
+  suggestionsList.innerHTML = html;
+
+//display just 3 suggested cities
+  for(let i=3; i<suggestionsList.childElementCount; i++){
+    if(suggestionsCity[i]){
+      suggestionsCity[i].style.display = "none";
+    }
+  }
+}
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
