b14feff214072a865395dcceae291c6d1c082f02
JacobFeat <jakubk198@gmail.com>
2021-04-02 16:24:37 +0200

    add hover and click events

--- listPageJs.js
+++ listPageJs.js
@@ -1,7 +1,21 @@
-// const searchInput = document.querySelector(".search");
-// const lookupIcon = document.querySelector(".lookup-icon");
-//
-// searchInput.addEventListener('click', function(){
-//   searchInput.classList.add('search-active');
-//   lookupIcon.classList.add('search-active');
-// });
+const searchInput = document.querySelector(".search");
+const lookupIcon = document.querySelector(".lookup-icon");
+const lookupAndMap = document.querySelector(".lookup-and-map");
+
+
+//expand search input
+searchInput.addEventListener('click', function(){
+  searchInput.classList.add('search-active');
+  lookupIcon.classList.add('lookup-icon-active');
+});
+
+lookupIcon.addEventListener('click', function(){
+  searchInput.classList.add('search-active');
+  lookupIcon.classList.add('lookup-icon-active');
+});
+
+//hover search icon
+lookupAndMap.addEventListener('mouseover', function(e){
+  e.target.className ==  "material-icons lookup-icon" ? searchInput.classList.add("search-hover") : searchInput.classList.remove("search-hover");
+
+})
