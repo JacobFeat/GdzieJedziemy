d8fbfd4f99b267579c458c29a18a2329cbd36c9f
JacobFeat <jakubk198@gmail.com>
2021-04-02 11:42:05 +0200

    add primary layout to ListPage v2

---
+++ listPageJs.js
@@ -0,0 +1,7 @@
+// const searchInput = document.querySelector(".search");
+// const lookupIcon = document.querySelector(".lookup-icon");
+//
+// searchInput.addEventListener('click', function(){
+//   searchInput.classList.add('search-active');
+//   lookupIcon.classList.add('search-active');
+// });
