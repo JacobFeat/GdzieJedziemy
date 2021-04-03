344dbd2b121f5bb77937da379f9bca5fed9c42c6
JacobFeat <jakubk198@gmail.com>
2021-04-03 14:40:54 +0200

    searching feature is done

--- listPageJs.js
+++ listPageJs.js
@@ -4,21 +4,61 @@ const lookupIcon = document.querySelector(".lookup-icon");
 const lookupAndMap = document.querySelector(".lookup-and-map");
 const suggestionsCity = suggestionsList.children;
 
+const myPlace = {
+  lat: 49.092
+}
+
 document.addEventListener('mouseover', (e) => {
   e.target.className === "search" ? searchInput.classList.add("search-hover") : searchInput.classList.remove("search-hover");
 });
 
-//expand and collapse search input
 document.addEventListener('click', (e) => {
-  if (e.target.className === "search" || e.target.className === "search search-active" || e.target.className === "search search-hover") {
+  //expand and collapse search input
+  if(e.target.classList.contains('search')){
     searchInput.classList.add('search-active');
     lookupIcon.classList.add('lookup-icon-active');
-  } else {
+  }
+  else{
     searchInput.classList.remove('search-active');
     lookupIcon.classList.remove('lookup-icon-active');
-    searchInput.value = "";
+
   }
-});
+
+  if(e.target.closest('li')){
+    // TODO: add popup feature about particular object
+  }
+
+  if(e.target.classList.contains('name')){
+    // add main-color bullet to clicked suggested name
+    let dotForLi = document.createElement('span');
+    dotForLi.setAttribute('class', 'dot-for-li');
+    e.target.appendChild(dotForLi);
+    //replace search input value by clicked suggested name
+    searchInput.value = e.target.textContent;
+    const currentName = e.target.textContent;
+
+    //leave just one active suggested city feature
+    const suggestionsCityArray = Array.from(suggestionsCity);
+
+    if(e.target.textContent == currentName){
+      // make a function which return new array with name of cities
+      function findMyCity(place){
+        return place = searchInput.value;
+      }
+      const showMyCity = suggestionsCityArray.filter(findMyCity)
+                                              .map(place => {
+        return place.innerText;
+      });
+      const myCurrentCityIndex = showMyCity.indexOf(currentName);
+      for(let i=0; i<3; i++){
+        if(suggestionsCity[i]){
+          suggestionsCity[i].style.display = "none";
+          suggestionsCity[myCurrentCityIndex].style.display = "inline-block";
+        }
+      }
+    }
+  }
+}, false);
 
 //search city function
 let cities = [];
@@ -54,6 +94,7 @@ function displayMatches(){
       suggestionsCity[i].style.display = "none";
     }
   }
+
 }
 
 searchInput.addEventListener('change', displayMatches);
